779f909f62e0b4d7d590ddd21e33e835b13094aa
Emina Ljubijankic <147718356+Ljunijankic95@users.noreply.github.com>
2023-10-15 18:33:07 +0200

    WeightConverter
    This Python script defines two functions for converting weight values between pounds and kilograms. It provides a simple way to perform these conversions programmatically.

---
+++ weight_converter.py
@@ -0,0 +1,7 @@
+# Function for converting pounds to kilograms
+def lbs_to_kg(weight):
+    return weight * 0.45
+
+# Function for converting kilograms to pounds
+def kg_to_lbs(weight):
+    return weight / 0.45
